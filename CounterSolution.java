fc41b0bd1893e9276f9ca78f6d7b328b55cc1e6d
BugHunter-X <askzenai@gmail.com>
2024-12-17 02:04:07 +0100

    Add CounterSolution.java file

---
+++ CounterSolution.java
@@ -0,0 +1,32 @@
+public class Counter {
+    private int count = 0;
+
+    public synchronized void increment() {
+        count++;
+    }
+
+    public synchronized int getCount() {
+        return count;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        Counter counter = new Counter();
+        Thread t1 = new Thread(() -> {
+            for (int i = 0; i < 10000; i++) {
+                counter.increment();
+            }
+        });
+        Thread t2 = new Thread(() -> {
+            for (int i = 0; i < 10000; i++) {
+                counter.increment();
+            }
+        });
+        t1.start();
+        t2.start();
+        t1.join();
+        t2.join();
+        System.out.println("Count: " + counter.getCount());
+    }
+}
